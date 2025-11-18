543e07f56bddcb2c540f7bf43533eed3fb00fc5d
Jaylano <jaylanovanderveen@gmail.com>
2025-11-18 09:15:53 +0100

    fix: Rename postCall to apiCall and update API endpoint URL

--- src/Calls/calls.js
+++ src/Calls/calls.js
@@ -1,8 +1,8 @@
 
-const postCall = async (usedFunction, dataSend) => {
+const apiCall = async (usedFunction, dataSend) => {
     try {
         const response = await fetch(
-            "http://localhost/tandartspraktijkBackend/router/router.php",
+            "http://localhost/APKlaarAPI/router/router.php",
             {
                 method: "POST",
                 body: JSON.stringify({
